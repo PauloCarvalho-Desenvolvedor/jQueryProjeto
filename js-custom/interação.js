cddda09760863079c09f7381e7d19b9ef870584f
Paulo Carvalho <86626301+PauloCarvalho-Desenvolvedor@users.noreply.github.com>
2023-06-26 21:22:23 -0300

    primeiro projeto em jQuery em andamento

---
+++ js-custom/interação.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
+
+})
+
+$(document).ready(function() {
+    $('#lightSlider').lightSlider({
+    gallery:true,
+    minSlide:1,
+    maxSlide:1,
+    auto:true     
+    });  
+});
+
